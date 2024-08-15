992ae4e2d579c54b84b7bfad4a0f4f1367147914
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-08-15 10:52:20 +0530

    minor fixes

--- app/page.js
+++ app/page.js
@@ -1,112 +1,237 @@
+"use client";
+import React from "react";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="flex h-screen flex-col items-center justify-between bg-[#1e1d1d]">
+      <div className=" flex flex-row justify-between w-full h-screen">
+        <div className="flex flex-col items-center justify-center w-1/2 h-screen bg-[#000000] shadow-4xl z-40 relative overflow-hidden">
+          <div className=" h-[50vh] w-[50vh] absolute -right-[20vh] -bottom-[20vh] ">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/blob.png"
+              layout="fill"
+              objectFit="contain"
+              className=" opacity-40"
             />
-          </a>
+          </div>
+
+          <div className=" h-[50vh] w-[50vh] absolute -left-[20vh] -bottom-[20vh] ">
+            <Image
+              src="/blob.png"
+              layout="fill"
+              objectFit="contain"
+              className=" opacity-40"
+            />
+          </div>
+
+          <div className=" h-[50vh] w-[50vh] top-[10vh] absolute ">
+            <Image
+              src="/blob.png"
+              layout="fill"
+              objectFit="contain"
+              className=" opacity-40"
+            />
+          </div>
+          <h2 className="text-[4rem] z-40 text-white font-bold">Ai.gnosis</h2>
+          <p className="text-[1rem] z-40 text-white">
+            Please take the assesment to begin with the diagnosis{" "}
+          </p>
+          <div className=" w-9/12 mt-[5vh] z-40 py-4 px-4 border-[1px] border-[#c7c7c7]/40 rounded-xl bg-[#121212] flex flex-col items-center">
+            <div className=" w-full flex flex-row justify-around">
+              <div className=" flex flex-col items-start">
+                <p className="text-xs text-white/80">Assessment Duration</p>
+                <p className="text-xl text-white">20 mins</p>
+              </div>
+
+              <div className=" flex flex-col items-start">
+                <p className="text-xs text-white/80">No. of Questions</p>
+                <p className="text-xl text-white">10</p>
+              </div>
+
+              <div className=" flex flex-col items-start">
+                <p className="text-xs text-white/80">Mode</p>
+                <p className="text-xl text-white">Online</p>
+              </div>
+            </div>
+
+            <button className="w-1/2 mt-8 border-[0.5px] border-[#b8aeba55] transition-all duration-150 hover:scale-95 py-2 px-4 bg-[#3b0d46] text-white rounded-lg">
+              Start Assessment
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="flex flex-col items-center justify-start w-1/2 bg-[#1a1a1a] py-8 px-[5%] overflow-y-scroll">
+          <div className=" flex flex-col items-start h-[30vh] w-full mt-[30vh] ">
+            <h2 className="text-[2rem] leading-[2rem] text-white font-bold">
+              Welcome to Ai.gnosis early detection screener
+            </h2>
+            <p className="text-[1rem] text-white mt-[2.5vh]">
+              Ai.gnosis is an online platform that helps you to detect early
+              signs of developmental disorders in children.
+            </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <button className="w-1/2 mt-8 border-[0.5px] border-[#b8aeba55] transition-all duration-150 hover:scale-95 py-2 px-4 bg-[#3b0d46] text-white rounded-lg">
+              {"Let's get started"}
+            </button>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className=" flex flex-col items-start h-[30vh] w-full mt-[15vh]">
+            <h2 className="text-[2rem] leading-[2rem] text-white font-bold">
+              Prerequisites:
+            </h2>
+            <p className="text-[1rem] text-white mt-[2.5vh]">
+              1. A quiet room with no distractions
+            </p>
+            <p className="text-[1rem] text-white mt-[0.5vh]">
+              2. A device with a stable internet connection
+            </p>
+            <p className="text-[1rem] text-white mt-[0.5vh]">
+              3. Working camera and microphone
+            </p>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className=" flex flex-col items-start h-full w-full mt-[25vh]">
+            <h2 className="text-[2rem] leading-[2rem] text-white font-bold">
+              Registration Form
+            </h2>
+            <p className="text-[1rem] text-white mt-2">
+              Please fill the form below to begin the assessment
+            </p>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full mt-4">
+              <div className=" grid grid-cols-2 gap-4">
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    {...register("name", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  />
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">
+                    Date of birth
+                  </label>
+                  <input
+                    type="date"
+                    name="dob"
+                    {...register("dob", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  />
+                </div>
+
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">
+                    Age in days
+                  </label>
+                  <input
+                    type="number"
+                    name="ageInMonths"
+                    {...register("ageInMonths", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  />
+                </div>
+
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">
+                    Age in months
+                  </label>
+                  <input
+                    type="number"
+                    name="ageInMonths"
+                    {...register("ageInMonths", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  />
+                </div>
+
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">
+                    Age in years
+                  </label>
+                  <input
+                    type="number"
+                    name="ageInYears"
+                    {...register("ageInYears", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  />
+                </div>
+
+                <div className="flex flex-col items-start w-full">
+                  <label className="text-white text-[0.8rem]">
+                    Assessment Type
+                  </label>
+                  <select
+                    name="assessmentType"
+                    {...register("assessmentType", { required: true })}
+                    className="w-full py-2 px-2 border-[0.5px] border-[#b8aeba55] rounded-lg bg-[#121212] text-white"
+                  >
+                    <option value="">Select Assessment Type</option>
+                    <option value="Type 1">Type 1</option>
+                    <option value="Type 2">Type 2</option>
+                    <option value="Type 3">Type 3</option>
+                  </select>
+                </div>
+              </div>
+              <div className=" mt-6 flex flex-row justify-start items-center">
+                <input type={"checkbox"} className=" h-4 w-4" />
+                <label className="text-white text-sm ml-2">
+                  Consent form filled
+                </label>
+              </div>
+              <div className=" mt-2 flex flex-row justify-start items-center">
+                <input type={"checkbox"} className=" h-4 w-4" />
+                <label className="text-white text-sm ml-2">
+                  I agree to the terms and conditions
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="w-1/2 mt-8 border-[0.5px] border-[#b8aeba55] transition-all duration-150 hover:scale-95 py-2 px-4 bg-[#3b0d46] text-white rounded-lg"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+
+          <div className=" flex flex-col items-start h-full w-full mt-[25vh]">
+            <h2 className="text-[2rem] leading-[2rem] text-white font-bold">
+              Assessment Instructions
+            </h2>
+
+            <p className="text-[1rem] text-white mt-2">
+              Please read the instructions below before starting the assessment
+            </p>
+
+            <div className="flex flex-col items-start mt-4 gap-y-4">
+              <p className="text-sm text-white">
+                1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </p>
+              <p className="text-sm text-white">
+                2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </p>
+              <p className="text-sm text-white">
+                3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </p>
+              <p className="text-sm text-white">
+                4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
